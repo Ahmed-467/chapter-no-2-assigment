6a629f57b6981dbef914d9023f092392e89c6641
Ahmed-467 <hplaptop03421@gmail.com>
2024-02-28 23:24:34 +0500

    chapter no 2 created

---
+++ chapter no2/app.js
@@ -0,0 +1,46 @@
+//qno1
+// var book = "A smarter \n way to learn JavaScript";
+// alert("I am trying to Learn from Book " + book);
+
+
+//qno2
+// var content = "Yah! i am ahmed nadeem";
+// document.write(content);
+
+//qno3
+// var email = "hplaptop2008@gmail.com";
+//         alert("My email address is " + email);
+
+//qno4
+// var messasage = 'helloworld'
+// alert('messasage')
+
+//qno5
+// var username = 'ahmed nadeem';
+// console.log("my name is" + username)
+
+
+//qno6
+// var userName="PIZZA \n PIZZ \n PIZ \n PI \n P";
+//    alert(userName);
+
+
+
+   //qno7
+//    var userName = "Ahmed Nadeem";
+//         var Age = "17";
+//         var course = " Web and  mobile application development";
+//         alert(userName);
+//         alert(Age);
+//         alert(course);
+
+
+//qno8
+// var style = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(style);
+
+
+
+
+
+
